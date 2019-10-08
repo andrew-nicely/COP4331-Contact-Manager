a73530eaaa8f2e59e3b06c60caa0785385f3381a
Junejae <mykjj1029@gmail.com>
2019-10-07 23:06:54 -0400

    port is changed 4000->4404

---
+++ project/backend/users.model.js
@@ -0,0 +1,27 @@
+// users.model.js
+// Junejae Kim
+// COP 4331
+// Project: Contact Manager
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+    userID: {
+        type: String
+    },
+    userPW: {
+        type: String
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailAddress: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('User', User);
