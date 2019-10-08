6edc68d6c527cc4a530af76389ff3fbdee8ee9fa
Junejae <mykjj1029@gmail.com>
2019-10-07 23:09:16 -0400

    trimmed

--- practice/frontend/src/SignOutButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SignOutButton = ({onRouteChange, text}) => {
-	return(
-		<p onClick={() => onRouteChange('testsignout')}className="f6 link dim black db pointer">{text}</p>
-	)
-}
-
-export default SignOutButton;
